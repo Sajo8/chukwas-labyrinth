a117a623a99d0d7727cb8e8c943e83534582f166
Sajo8 <andrewsplin@protonmail.com>
2019-06-26 17:43:20 -0700

    Add comments to script

--- TurtleIcon.gd
+++ TurtleIcon.gd
@@ -2,11 +2,12 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Start rotating the icon immediately
 	$Sprite/AnimationPlayer.play("Rotating TurtleCoin Icon")
 
 func _on_Area2D_body_entered(body):
+	# When the coin is hit, get rid of it
+	"""
+	Still want to do something more with the powerup, not yet decided.
+	"""
 	queue_free()
-
-"""
-Still want to do something more with the powerup, not yet decided.
-"""
