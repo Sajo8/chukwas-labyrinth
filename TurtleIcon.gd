fb5f66bd02850367004b954f9e8a9ae0c0ea83b2
Sajo8 <andrewsplin@protonmail.com>
2019-07-01 16:21:21 -0700

    Adjust turtleicon animation
    - change name
    - set flag directly in the animation ui to start the animation once it's ready
    - remove code associated with that

--- TurtleIcon.gd
+++ TurtleIcon.gd
@@ -1,10 +1,5 @@
 extends Area2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	# Start rotating the icon immediately
-	$Sprite/AnimationPlayer.play("Rotating TurtleCoin Icon")
-
 func _on_Area2D_body_entered(body):
 	# When the coin is hit, get rid of it
 	"""
