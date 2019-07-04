c818e863d503e0d9a8de756e6ea376b503906e51
Sajo8 <andrewsplin@protonmail.com>
2019-07-03 23:51:41 -0700

    Make powerup increase in size and fade away once collected
    merci kidscancode

--- Powerup.gd
+++ Powerup.gd
@@ -1,7 +1,19 @@
 extends Area2D
 
-func _on_Area2D_body_entered(body):
-	# When the coin is hit, get rid of it
+func _ready():
+	$Effect.interpolate_property($Sprite, 'scale',
+		$Sprite.get_scale(), Vector2(2, 2), 0.5,
+		Tween.TRANS_QUAD, Tween.EASE_OUT)
+	
+	$Effect.interpolate_property($Sprite, 'modulate',
+		get_modulate(), Color(0, 0, 0, 0), 0.5,
+		Tween.TRANS_QUAD, Tween.EASE_OUT)
+
+func _on_Powerup_body_entered(body):
+	if body.get_name() == "Player":
+		$Effect.start()
+
+func _on_Effect_tween_completed(object, key):
 	"""
 	Still want to do something more with the powerup, not yet decided.
 	"""
