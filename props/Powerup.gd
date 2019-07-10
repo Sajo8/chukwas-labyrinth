69bc664ec54f416c2dd8cb3c522cbee775956701
Sajo8 <andrewsplin@protonmail.com>
2019-07-09 17:47:04 -0700

    Re-organize files

--- props/Powerup.gd
+++ props/Powerup.gd
@@ -1,18 +1,22 @@
 extends Area2D
 
+signal powerup_grabbed
+
 func _ready():
+	# Increase size
 	$Effect.interpolate_property($Sprite, 'scale',
 		$Sprite.get_scale(), Vector2(2, 2), 0.5,
-		Tween.TRANS_QUAD, Tween.EASE_OUT)
-	
+		Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	# Fade out
 	$Effect.interpolate_property($Sprite, 'modulate',
 		Color(1, 1, 1, 1), Color(0, 0, 0, 0), 0.5,
-		Tween.TRANS_QUAD, Tween.EASE_OUT)
+		Tween.TRANS_LINEAR, Tween.EASE_OUT)
 
 func _on_Powerup_body_entered(body):
 	if body.get_name() == "Player":
 		# Remove the collision shapes to prevent extra collisions during the time the effect is taking place.
 		shape_owner_clear_shapes(get_shape_owners()[0])
+		emit_signal("powerup_grabbed")
 		$Effect.start()
 
 func _on_Effect_tween_completed(object, key):
