028e36ad6e55c4aa3ce3fb071edbd80a8e44e25e
Sajo8 <andrewsplin@protonmail.com>
2019-07-11 12:39:21 -0700

    Prevent pillar base from moving
    FINALLY! After 1 week. Base stays constant, pillar moves up and down. Hits player, etc etc. Fully extended so easy to move around in editor.
    Note: tween still glitchy.
    I changed it to a staticbody, if that makes issues later i'll change it to kinematic but it works fines for now

--- props/Squasher.gd
+++ props/Squasher.gd
@@ -1,4 +1,10 @@
-extends KinematicBody2D
+extends StaticBody2D
+
+# Note:
+# The tween glitches out at the beginning
+# After going down and then up once (starts in fully extended position)
+# It doesn't go down properly; abrupt goes up again from down position
+# But doesn't happen again so whatever
 
 # Node refs
 
@@ -8,6 +14,9 @@ onready var tween = $Tween
 onready var squasher = $Squasher
 onready var squasher_collision = $SquasherCollisionShape
 
+onready var original_squasher_collision_pos = squasher_collision.position
+onready var squashed_squasher_collision_pos = Vector2(0, original_squasher_collision_pos.y + 48.75)
+
 # Top-part of pillar
 onready var squashertop = $SquasherTop
 onready var squashertop_collision = $TopCollisionShape
@@ -15,7 +24,7 @@ onready var squashertop_collision = $TopCollisionShape
 # Where the top-part started out
 onready var original_pos = squashertop.position
 # Where the top-part will end up after the squasher shrinks
-onready var squashed_pos = Vector2(0, original_pos.y + 49)
+onready var squashed_pos = Vector2(0, original_pos.y + 98)
 
 func _ready():
 	
@@ -36,6 +45,13 @@ func shrink_squasher():
 	
 	tween.interpolate_property(squasher_collision, "scale", 
 		Vector2(1.0, 1.0), Vector2(1.0, 0.1), duration,
+		Tween.TRANS_SINE, Tween.EASE_IN_OUT, idle_duration)
+	
+	# The pillar automatically scales from the top only because the pivot point has been set to the bottom-most point. No such option exists for a CollisionShape, so this exists
+	# Change position of collisionshape so that it matches the actual column
+	# Too high to proper height
+	tween.interpolate_property(squasher_collision, "position", 
+		original_squasher_collision_pos, squashed_squasher_collision_pos, duration,
 		Tween.TRANS_SINE, Tween.EASE_IN_OUT, idle_duration)
 
 
@@ -62,6 +78,13 @@ func maximise_squasher():
 	
 	tween.interpolate_property(squasher_collision, "scale", 
 		null, Vector2(1.0, 1.0), duration,
+		Tween.TRANS_CIRC, Tween.EASE_OUT, idle_duration)
+	
+	# The pillar automatically scales from the top only because the pivot point has been set to the bottom-most point. No such option exists for a CollisionShape, so this exists
+	# Change position of collisionshape so that it matches the actual column
+	# Too low to proper height
+	tween.interpolate_property(squasher_collision, "position", 
+		squashed_squasher_collision_pos, original_squasher_collision_pos, duration,
 		Tween.TRANS_CIRC, Tween.EASE_OUT, idle_duration)
 
 
