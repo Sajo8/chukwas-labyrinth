c52604263a200936862e99ad272ff7f0fcbe5aa9
Sajo8 <andrewsplin@protonmail.com>
2019-07-13 13:18:35 -0700

    allow to dynamically change side to check for collisions
    if you rotate the thing then just change the squash side

--- props/Squasher.gd
+++ props/Squasher.gd
@@ -13,6 +13,9 @@ extends StaticBody2D
 
 signal game_over
 
+export(String, "top", "down", "left", "right") var squash_side = "top" #setget set_squash_side
+var squash_side_vector : Vector2
+
 # Node refs
 
 onready var tween = $Tween
@@ -35,88 +38,102 @@ onready var original_pos = squashertop.position
 # Where the top-part will end up after the squasher shrinks
 onready var squashed_pos = Vector2(0, original_pos.y + 98)
 
+func set_squash_side():
+	# Set the vector we should check for collisions from the player based on what's set in the editor
+	# default top
+	if squash_side == "top":
+		squash_side_vector = Vector2(0, -1)
+	elif squash_side == "down":
+		squash_side_vector = Vector2(0, 1)
+	elif squash_side == "left":
+		squash_side_vector = Vector2(-1, 0)
+	else:
+		squash_side_vector = Vector2(1, 0)
+
 func _ready():
-	
+
+	set_squash_side()
+
 	var player = get_tree().get_nodes_in_group("player")[0]
-	
 	player.connect("hit_squasher", self, "_on_hit_squasher")
-	
+
 	shrink_squasher()
 	yield(tween, "tween_completed")
 	maximise_squasher()
 	yield(tween, "tween_completed")
 
 func shrink_squasher():
-	
+
 	var idle_duration = 3
 	var duration = 2
 
-	# Reduce size of column    
-	tween.interpolate_property(squasher, "scale", 
+	# Reduce size of column
+	tween.interpolate_property(squasher, "scale",
 		Vector2(1.0, 1.0), Vector2(1.0, 0.1), duration,
 		Tween.TRANS_SINE, Tween.EASE_IN_OUT, idle_duration)
-	
-	tween.interpolate_property(squasher_collision, "scale", 
+
+	tween.interpolate_property(squasher_collision, "scale",
 		Vector2(1.0, 1.0), Vector2(1.0, 0.1), duration,
 		Tween.TRANS_SINE, Tween.EASE_IN_OUT, idle_duration)
-	
+
 	# The pillar automatically scales from the top only because the pivot point has been set to the bottom-most point. No such option exists for a CollisionShape, so this exists
 	# Change position of collisionshape so that it matches the actual column
 	# Too high to proper height
-	tween.interpolate_property(squasher_collision, "position", 
+	tween.interpolate_property(squasher_collision, "position",
 		original_squasher_collision_pos, squashed_squasher_collision_pos, duration,
 		Tween.TRANS_SINE, Tween.EASE_IN_OUT, idle_duration)
 
 
 	# Change position of top portion to match
-	tween.interpolate_property(squashertop, "position", 
+	tween.interpolate_property(squashertop, "position",
 		original_pos, squashed_pos, duration,
 		Tween.TRANS_SINE, Tween.EASE_IN_OUT, idle_duration)
-	
-	tween.interpolate_property(squashertop_collision, "position", 
+
+	tween.interpolate_property(squashertop_collision, "position",
 		original_pos, squashed_pos, duration,
 		Tween.TRANS_SINE, Tween.EASE_IN_OUT, idle_duration)
 
 	tween.start()
 
 func maximise_squasher():
-	
+
 	var idle_duration = 2
 	var duration = 0.2
-	
-	# Increase size of column    
-	tween.interpolate_property(squasher, "scale", 
+
+	# Increase size of column
+	tween.interpolate_property(squasher, "scale",
 		null, Vector2(1.0, 1.0), duration,
 		Tween.TRANS_CIRC, Tween.EASE_OUT, idle_duration)
-	
-	tween.interpolate_property(squasher_collision, "scale", 
+
+	tween.interpolate_property(squasher_collision, "scale",
 		null, Vector2(1.0, 1.0), duration,
 		Tween.TRANS_CIRC, Tween.EASE_OUT, idle_duration)
-	
+
 	# The pillar automatically scales from the top only because the pivot point has been set to the bottom-most point. No such option exists for a CollisionShape, so this exists
 	# Change position of collisionshape so that it matches the actual column
 	# Too low to proper height
-	tween.interpolate_property(squasher_collision, "position", 
+	tween.interpolate_property(squasher_collision, "position",
 		squashed_squasher_collision_pos, original_squasher_collision_pos, duration,
 		Tween.TRANS_CIRC, Tween.EASE_OUT, idle_duration)
 
 
 	# Change position of top portion to match
-	tween.interpolate_property(squashertop, "position", 
+	tween.interpolate_property(squashertop, "position",
 		squashed_pos, original_pos, duration,
 		Tween.TRANS_CIRC, Tween.EASE_OUT, idle_duration)
-	
-	tween.interpolate_property(squashertop_collision, "position", 
+
+	tween.interpolate_property(squashertop_collision, "position",
 		squashed_pos, original_pos, duration,
 		Tween.TRANS_CIRC, Tween.EASE_OUT, idle_duration)
 
 	tween.start()
 
 func _on_hit_squasher(collision):
-	if collision.normal.y != -1: # Only continue if it hit the top
+
+	if collision.normal != squash_side_vector: # Only continue if it hit the side the squash part is
 		return
 	var current_tween_time = tween.tell()
-	
+
 	if current_tween_time >= 2 and current_tween_time <= 3.2: # Only check when it's fully extended
 		yield(tween, "tween_completed") # Wait for animation to finish
 		emit_signal("game_over")
