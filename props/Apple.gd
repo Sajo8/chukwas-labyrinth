ae9a4f25a2f1cace6f70bae5be13cde7f2831b51
Sajo8 <andrewsplin@protonmail.com>
2019-07-16 22:07:29 -0700

    Add tween effects to apple and watermelon powerups

--- props/Apple.gd
+++ props/Apple.gd
@@ -2,7 +2,22 @@ extends Area2D
 
 signal powerup_grabbed(type)
 
+func _ready():
+	# Increase size
+	$Effect.interpolate_property($Sprite, 'scale',
+		$Sprite.get_scale(), Vector2(2, 2), 0.6,
+		Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	# Fade out
+	$Effect.interpolate_property($Sprite, 'modulate',
+		Color(1, 1, 1, 1), Color(0, 0, 0, 0), 0.6,
+		Tween.TRANS_LINEAR, Tween.EASE_OUT)
+
 func _on_Apple_body_entered(body):
 	if body.get_name() == "Player":
-		emit_signal("powerup_grabbed", 'apple')
-		queue_free()
+		# Remove the collision shapes to prevent extra collisions during the time the effect is taking place.
+		shape_owner_clear_shapes(get_shape_owners()[0])
+		$Effect.start()
+
+func _on_Effect_tween_completed(object, key):
+	queue_free()
+	emit_signal("powerup_grabbed", 'apple')
