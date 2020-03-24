69d914c0fc1d1c8d16b2a6c5bb9f1c2c4ea08582
Andrew Splin <andrewsplin@protonmail.com>
2020-03-24 13:09:28 -0700

    check if dlc enabled on my server, add coins and spikes and fix spawn

--- gui/screens/OptionsScreen.gd
+++ gui/screens/OptionsScreen.gd
@@ -3,3 +3,25 @@ extends Control
 func _ready():
 	$MainMenu.grab_focus()
 
+	# if it's not already disabled (ie, they have all the dlc)
+	# then check the server to see if DLCs are currently enabled
+	if not $Buttons/DLC.disabled:
+		$Buttons/DLC.text = "Loading DLC..."
+		$Buttons/DLC.disabled = true
+
+		var err = $CheckDLCEnabled.request("https://testsajoeexpress.netlify.com/.netlify/functions/server/trtlapps/getDlcEnabled")
+		if err != OK:
+			$Buttons/DLC.text = "DLC currently disabled"
+			$Buttons/DLC.disabled = true
+
+func _on_CheckDLCEnabled_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray) -> void:
+	var json_result = JSON.parse(body.get_string_from_utf8()).result
+	if json_result['result']:
+		$Buttons/DLC.text = "Buy DLC"
+		$Buttons/DLC.disabled = false
+	else:
+		make_dlc_disabled()
+
+func make_dlc_disabled():
+	$Buttons/DLC.text = "DLC currently disabled"
+	$Buttons/DLC.disabled = true
